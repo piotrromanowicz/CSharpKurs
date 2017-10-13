11d04d0c59efafb9990333c98fa21a3a257757f0
Piotr <Piotr@ASUSW7>
2017-10-13 23:19:17 +0200

    Pierwszy Program Hello. Tablica args.

--- Hello/Program.cs
+++ Hello/Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Hello
 {
@@ -10,6 +6,12 @@ namespace Hello
     {
         static void Main(string[] args)
         {
+            foreach (var item in args)
+            {
+                Console.WriteLine("Witaj " + item);
+            }
+            Console.WriteLine(DateTime.Now.DayOfWeek);
+            Console.WriteLine(args[0] + " dzisiaj jest " + DateTime.Now.DayOfYear + " dzień roku.");
         }
     }
 }
