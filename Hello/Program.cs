ec7d8f4019e005051c3b4d06136f59c73862d83d
Piotr <Piotr@ASUSW7>
2017-10-20 19:10:35 +0200

    dodanoe wartosci

--- Hello/Program.cs
+++ Hello/Program.cs
@@ -6,17 +6,82 @@ namespace Hello
     {
         static void Main(string[] args)
         {
-            foreach (var item in args)
+            ValueTypes();
+
+            for (;;)
             {
-                Console.WriteLine("Witaj " + item);
+                Greeting();
+                Age();
+                Setting();
             }
-            Console.WriteLine(DateTime.Now.DayOfWeek);
-            Console.WriteLine(args[0] + " dzisiaj jest " + DateTime.Now.DayOfYear + " dzień roku.");
-            for (int i = 0; i <= 10; i++)
-            {
-                Console.WriteLine("Liczba " + i);
-            }
+        }
+
+        /// <summary>
+        /// max i min wartości zmiennych
+        /// </summary>
+        private static void ValueTypes()
+        {
+            int maxInt = int.MaxValue;
+            int minInt = int.MinValue;
+            float maxFloat = float.MaxValue;
+            float minFloat = float.MinValue;
+            long maxLong = long.MaxValue;
+            long minLong = long.MinValue;
+
+            Console.WriteLine("max wartość int: " + maxInt);
+            Console.WriteLine("min wartość int: " + minInt);
+            Console.WriteLine("max wartość float: " + maxFloat);
+            Console.WriteLine("min wartość float: " + minFloat);
+            Console.WriteLine("max wartość long: " + maxLong);
+            Console.WriteLine("min wartość long: " + minLong);
+        }
+
+        /// <summary>
+        /// ustawienia
+        /// </summary>
+        private static void Setting()
+        {
+            Console.ResetColor();
+            Console.WriteLine("Naciśnij dowolny klawisz");
             Console.ReadKey();
+            Console.Clear();
+        }
+        
+        /// <summary>
+        /// sprawdzenie wieku
+        /// </summary>
+        private static void Age()
+        {
+            Console.Write("Write, how old are you ? ");
+            int age;
+            bool result = int.TryParse(Console.ReadLine(), out age);
+
+            if (age <= 0)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Złe dane");
+            }
+            else if (age < 18)
+            {
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.WriteLine("You can drink milk only.");
+            }
+            else
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("You can drink beer and wine.");
+            }
+        }
+        
+        /// <summary>
+        /// powitanie 
+        /// </summary>
+        private static void Greeting()
+        {
+            Console.Write("Write, What your name: ");
+            string name = Console.ReadLine();
+
+            Console.WriteLine("Hello " + name);
         }
     }
 }
